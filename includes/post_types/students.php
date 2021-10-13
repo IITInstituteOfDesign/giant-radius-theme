66e98de5f81f885f6c467c5d233354b3eea89b34
Pete Croke <pete@petecroke.com>
2021-10-13 12:41:36 -0400

    remove debug output from student post type

--- includes/post_types/students.php
+++ includes/post_types/students.php
@@ -6,13 +6,9 @@ $student->supports = array('thumbnail');
 $student->has_archive = false;
 
 function generate_student_name( $post_id ) {
-	//var_dump($post_id);
-	//exit;
 	if (get_field('first_name', $post_id) && get_field('last_name', $post_id)):
 		$name = array( get_field('first_name', $post_id), get_field('last_name', $post_id) );
 		$name = implode( ' ', array_filter( $name ));
-
-		//remove_action( 'acf/save_post', 'generate_student_name', 20 );
 
 		wp_update_post( array(
 			'ID' => $post_id,
